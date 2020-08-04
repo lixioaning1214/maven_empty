a5937859dbc5e2a065c0eac6ed920f13d1b93697
李敢敢 <1336976407@qq.com>
2020-08-04 17:19:58 +0800

    这是一个maven空项目

---
+++ src/main/java/com/lxn/pojo/User.java
@@ -0,0 +1,51 @@
+package com.lxn.pojo;
+
+public class User {
+
+    private Integer uid;
+    private String uname;
+    private String password;
+    private String sex;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", uname='" + uname + '\'' +
+                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
